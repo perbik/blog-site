5acb22982b1fbfe66446ff99ce3e822b1c5a25ef
Fervicmar Lagman <fervicmardlagman@gmail.com>
2026-07-07 14:53:39 +0800

    chore(db): create migration files

---
+++ drizzle/20260707065011_create-posts-comments-tables/migration.sql
@@ -0,0 +1,19 @@
+CREATE TABLE "comments" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	"post_id" uuid NOT NULL,
+	"author_name" text NOT NULL,
+	"body" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "posts" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	"title" text NOT NULL,
+	"slug" text NOT NULL UNIQUE,
+	"image" text,
+	"tags" text[] DEFAULT ARRAY[]::text[] NOT NULL,
+	"body" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "comments" ADD CONSTRAINT "comments_post_id_posts_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE;
