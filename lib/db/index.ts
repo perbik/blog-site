69738025b122e0b2ae55a0973bec1eb65f666382
Fervicmar Lagman <fervicmardlagman@gmail.com>
2026-07-07 14:52:09 +0800

    chore(db): create connection to Neon

---
+++ lib/db/index.ts
@@ -0,0 +1,13 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import { relations } from "./schema";
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+	throw new Error("DATABASE_URL is not set");
+}
+
+const sql = neon(databaseUrl);
+
+export const db = drizzle({ client: sql, relations });
