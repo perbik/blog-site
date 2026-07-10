03621352c5407df3e941702e974a2febea524091
Fervicmar Lagman <fervicmardlagman@gmail.com>
2026-07-10 13:16:05 +0800

    chore(config): add picsum for the seeder

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "picsum.photos",
+			},
+		],
+	},
 	turbopack: {
 		root: __dirname,
 	},
